89e94c79609eaa4b1307411c064673290b703b36
sajitha00 <sajithageevinda.sd@gmail.com>
2025-04-26 22:29:32 +0530

    ajust path

--- hotel-villa-website/src/App.jsx
+++ hotel-villa-website/src/App.jsx
@@ -9,7 +9,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Hotel">
       <div className="App">
         <NavBar />
         <Routes>
